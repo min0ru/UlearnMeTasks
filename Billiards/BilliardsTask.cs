f21ec7cfbb179d9f7713a8f7fb3176fad9ab3a3c
min0ru <2010min0ru@gmail.com>
2022-05-23 16:31:57 +0300

    Solved Billiard task.

--- Billiards/BilliardsTask.cs
+++ Billiards/BilliardsTask.cs
@@ -5,15 +5,14 @@ namespace Billiards
     public static class BilliardsTask
     {
         /// <summary>
-        /// 
+        /// Найти угол отскока в радианах для данных угла направления шара и наклона стены.
         /// </summary>
         /// <param name="directionRadians">Угол направления движения шара</param>
         /// <param name="wallInclinationRadians">Угол</param>
         /// <returns></returns>
         public static double BounceWall(double directionRadians, double wallInclinationRadians)
         {
-            //TODO
-            return 0.0;
+            return 2 * wallInclinationRadians - directionRadians;
         }
     }
 }
